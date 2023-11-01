f6af1cac708c696ed9ac38bdf691aae77f40dffd
Othi <mnpq.raven@gmail.com>
2023-11-01 23:16:37 +0700

    probably stable tailwind setup

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Card } from "ui";
+
 export default function Page() {
-  return <main className="font-bold">hello docs</main>;
+  return (
+    <main className="font-bold">
+      hello docs <br />
+      <Card title="hi" href="/">
+        hello
+      </Card>
+    </main>
+  );
 }
--- packages/ui/card.tsx
+++ packages/ui/card.tsx
@@ -10,18 +10,20 @@ export function Card({
   title: string;
   children: React.ReactNode;
   href: string;
-}): JSX.Element {
+}) {
   return (
-    <a
-      className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      <h2>
-        {title} <span>-&gt;</span>
-      </h2>
-      <p>{children}</p>
-    </a>
+    <div className="border rounded-md p-2 bg-red-300">
+      <a
+        className={className}
+        href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <h2>
+          {title} <span>-&gt;</span>
+        </h2>
+        <p>{children}</p>
+      </a>
+    </div>
   );
 }
