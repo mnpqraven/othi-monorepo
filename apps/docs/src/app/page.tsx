86b16f9f203854dbd2d153d1c56d295e98369190
Othi <mnpq.raven@gmail.com>
2023-11-02 17:19:14 +0700

    chore

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,9 +1,14 @@
+import { env } from "../env.mjs";
 import { Card } from "ui";
+import { HELLO } from "@docs/app/test";
 
 export default function Page() {
+  const test = HELLO;
+
   return (
     <main className="font-bold">
       hello docs <br />
+      {env.SECRET} {test}
       <Card title="hi" href="/">
         hello
       </Card>
