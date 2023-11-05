e8a4ddbbf9fb353a6c3915b784464246c2c30a40
Othi <mnpq.raven@gmail.com>
2023-11-05 15:34:57 +0700

    clear example repos

--- apps/db-center/src/app/_trpc/serverClient.ts
+++
@@ -1,6 +0,0 @@
-import { httpBatchLink } from "@trpc/client";
-import { appRouter } from "protocol/trpc";
-
-export const server = appRouter.createCaller({
-  links: [httpBatchLink({ url: "/api" })],
-});
