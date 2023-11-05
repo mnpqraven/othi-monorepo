677041b7abff22d6a54e05cb95f0fee11fef0b56
Othi <mnpq.raven@gmail.com>
2023-11-05 20:44:30 +0700

    docker

--- crates/nas-ws/build.rs
+++ crates/nas-ws/build.rs
@@ -1,7 +1,11 @@
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(debug_assertions)]
     let list = std::fs::read_dir("../../packages/protocol/proto").unwrap();
+    #[cfg(not(debug_assertions))]
+    let list = std::fs::read_dir("./packages/protocol/proto").unwrap();
+
     let list: Vec<PathBuf> = list.into_iter().map(|e| e.unwrap().path()).collect();
 
     for path in list {
