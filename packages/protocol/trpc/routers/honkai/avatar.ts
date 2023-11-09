b168615e41918ce3f7ad7ff21164e4645607aa00
Othi <mnpq.raven@gmail.com>
2023-11-09 22:39:40 +0700

    chore

--- packages/protocol/trpc/routers/honkai/avatar.ts
+++ packages/protocol/trpc/routers/honkai/avatar.ts
@@ -10,28 +10,26 @@ export const avatarRouter = router({
     async () =>
       (await db.select().from(avatars).all()) satisfies Awaited<AvatarSchema[]>
   ),
-  signatures: router({
-    by: publicProcedure
-      .input(CharId.extend({ skill: z.boolean().default(true) }))
-      .query(async ({ input }) =>
-        db.query.avatars
-          .findFirst({
-            where: (avatar, { eq }) => eq(avatar.id, input.charId),
-            columns: {},
-            with: {
-              signature: {
-                columns: {},
-                with: {
-                  lightCone: {
-                    with: {
-                      skill: input.skill || undefined,
-                    },
+  signatures: publicProcedure
+    .input(CharId.extend({ skill: z.boolean().default(true) }))
+    .query(async ({ input }) =>
+      db.query.avatars
+        .findFirst({
+          where: (avatar, { eq }) => eq(avatar.id, input.charId),
+          columns: {},
+          with: {
+            signature: {
+              columns: {},
+              with: {
+                lightCone: {
+                  with: {
+                    skill: input.skill || undefined,
                   },
                 },
               },
             },
-          })
-          .then((data) => data?.signature.flatMap((e) => e.lightCone) ?? [])
-      ),
-  }),
+          },
+        })
+        .then((data) => data?.signature.flatMap((e) => e.lightCone) ?? [])
+    ),
 });
