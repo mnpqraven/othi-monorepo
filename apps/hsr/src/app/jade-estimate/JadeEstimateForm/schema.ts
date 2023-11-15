2ef2ff1cd72f47201011e58b89047c54dc46a67e
Othi <mnpq.raven@gmail.com>
2023-11-15 17:00:06 +0700

    chore

--- apps/hsr/src/app/jade-estimate/JadeEstimateForm/schema.ts
+++ apps/hsr/src/app/jade-estimate/JadeEstimateForm/schema.ts
@@ -1,31 +1,25 @@
 import { BattlePassType, EqTier, Server } from "protocol/ts";
 import * as z from "zod";
 
-export const schema = z.object(
-  {
-    untilDate: z.object(
-      {
-        day: z.number(),
-        month: z.number(),
-        year: z.number(),
-      },
-      { required_error: "Required field" }
-    ),
-    battlePass: z.object({
-      battlePassType: z.nativeEnum(BattlePassType),
-      currentLevel: z.number().nonnegative(),
-    }),
-    railPass: z.object({
-      useRailPass: z.boolean(),
-      daysLeft: z.number().nonnegative(),
-    }),
-    server: z.nativeEnum(Server),
-    eq: z.nativeEnum(EqTier),
-    moc: z.number().nonnegative(),
-    mocCurrentWeekDone: z.boolean(),
-    currentRolls: z.number().nonnegative().optional(),
-    currentJades: z.number().nonnegative().optional(),
-    dailyRefills: z.number().nonnegative().optional(),
-  },
-  { required_error: "jade form " }
-);
+export const schema = z.object({
+  untilDate: z.object({
+    day: z.number(),
+    month: z.number(),
+    year: z.number(),
+  }),
+  battlePass: z.object({
+    battlePassType: z.nativeEnum(BattlePassType),
+    currentLevel: z.number().nonnegative(),
+  }),
+  railPass: z.object({
+    useRailPass: z.boolean(),
+    daysLeft: z.number().nonnegative(),
+  }),
+  server: z.nativeEnum(Server),
+  eq: z.nativeEnum(EqTier),
+  moc: z.number().nonnegative(),
+  mocCurrentWeekDone: z.boolean(),
+  currentRolls: z.number().nonnegative().optional(),
+  currentJades: z.number().nonnegative().optional(),
+  dailyRefills: z.number().nonnegative().optional(),
+});
