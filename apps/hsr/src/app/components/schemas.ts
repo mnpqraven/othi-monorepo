9be390ae6eba7c78c983775105313fbc625250b8
Othi <mnpq.raven@gmail.com>
2023-11-15 14:55:15 +0700

    can't wake up

--- apps/hsr/src/app/components/schemas.ts
+++ apps/hsr/src/app/components/schemas.ts
@@ -1,13 +1,20 @@
 import * as z from "zod";
 
-export const dateToISO = z.date().transform((e) => ({
-  day: e.getDate(),
-  month: e.getMonth() + 1,
-  year: e.getUTCFullYear(),
-}));
+export const dateToISO = z
+  .date({ required_error: "dateToISO" })
+  .transform((e) => ({
+    day: e.getDate(),
+    month: e.getMonth() + 1,
+    year: e.getUTCFullYear(),
+  }));
 
-export const objToDate = z.object({
-  day: z.number(),
-  month: z.number(),
-  year: z.number()
-}).transform((e) => new Date(e.year, e.month - 1, e.day));
+export const objToDate = z
+  .object(
+    {
+      day: z.number(),
+      month: z.number(),
+      year: z.number(),
+    },
+    { required_error: "objToDate" }
+  )
+  .transform((e) => new Date(e.year, e.month - 1, e.day));
