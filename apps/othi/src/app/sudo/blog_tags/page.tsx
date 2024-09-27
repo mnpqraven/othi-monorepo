9429fe7b37f40e9f293ac5b36543843f1db53aa5
Othi <mnpq.raven@gmail.com>
2024-09-27 15:58:45 +0700

    ts

--- apps/othi/src/app/sudo/blog_tags/page.tsx
+++ apps/othi/src/app/sudo/blog_tags/page.tsx
@@ -2,14 +2,12 @@ import { HydrateClient } from "protocol/trpc/react/server";
 import { BlogTagTable } from "./BlogTagTable";
 import { BlogTagForm } from "./BlogTagForm";
 
-export default function Page() {
+export default async function Page() {
   return (
-    <HydrateClient>
-      <div className="flex flex-col gap-2">
-        <BlogTagForm />
+    <div className="flex flex-col gap-2">
+      <BlogTagForm />
 
-        <BlogTagTable />
-      </div>
-    </HydrateClient>
+      <BlogTagTable />
+    </div>
   );
 }
