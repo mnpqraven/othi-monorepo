816df94b3c51383da265bca996edd9ab67b947f9
Othi <mnpq.raven@gmail.com>
2024-10-01 10:28:14 +0700

    ts

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -7,8 +7,12 @@ export const env = createEnv({
     OTHI_GITHUB_ID: z.string(),
     OTHI_GITHUB_SECRET: z.string(),
     GITHUB_IDENT: z
-      .number({ message: "must be number" })
-      .or(z.string({ message: "must be string" }))
+      .string({ message: "must be string" })
+      .transform((e) => {
+        console.log("debug", e);
+        return e;
+      })
+      .or(z.number({ message: "must be number" }))
       .pipe(z.coerce.number({ message: "throw on coercion" })),
   },
   client: {},
