ff23c671c4ca42561529581ff35de3f5b27d6463
Othi <mnpq.raven@gmail.com>
2024-10-01 10:53:35 +0700

    naming

--- packages/auth/env.ts
+++ packages/auth/env.ts
@@ -8,7 +8,7 @@ export const env = createEnv({
     NEXTAUTH_SECRET: z.string(),
     OTHI_GITHUB_ID: z.string(),
     OTHI_GITHUB_SECRET: z.string(),
-    GITHUB_IDENT: z
+    AUTH_GITHUB_IDENT: z
       .string({ message: "must be string" })
       .transform((e) => {
         console.log("debug", e);
