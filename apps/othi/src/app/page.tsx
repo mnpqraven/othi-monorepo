b079d4cc6ff4c433856e15ea6dceba07bceef364
Othi <mnpq.raven@gmail.com>
2024-10-07 12:01:17 +0700

    test build cmd

--- apps/othi/src/app/page.tsx
+++ apps/othi/src/app/page.tsx
@@ -5,11 +5,6 @@ export default function Page() {
   return (
     <HydrateClient>
       <Authed />
-
-      <div className="h-96 rounded-md border">hello</div>
-      <div className="h-96 rounded-md border">hello</div>
-      <div className="h-96 rounded-md border">hello</div>
-      <div className="h-96 rounded-md border">hello</div>
     </HydrateClient>
   );
 }
