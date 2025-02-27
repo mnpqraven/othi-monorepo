b2d27e6d5ef7b73c61a14209c4d459077c8113ad
Othi <mnpq.raven@gmail.com>
2025-02-27 19:17:32 +0700

    chore

--- apps/othi/src/app/api/trpc/[trpc]/route.ts
+++ apps/othi/src/app/api/trpc/[trpc]/route.ts
@@ -4,22 +4,16 @@ import type { NextRequest } from "next/server";
 import { appRouter } from "protocol/trpc";
 import { createTRPCContext } from "protocol/trpc/trpc";
 
-/**
- * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
- * handling a HTTP request (e.g. when you make requests from Client Components).
- */
-const createContext = (req: NextRequest) =>
-  createTRPCContext({
-    headers: req.headers,
-    cookies,
-  });
-
 const handler = (req: NextRequest) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => createContext(req),
+    createContext: () =>
+      createTRPCContext({
+        headers: req.headers,
+        cookies,
+      }),
   });
 
 export { handler as GET, handler as POST };
